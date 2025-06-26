62e12c84148471dd5a87db9541ae4086d1b2bc74
zanewnch <zanewnch@gmail.com>
2025-06-26 17:48:18 +0000

    Create a new react project for github page.

---
+++ githubPage/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  css: {
+    postcss: "./postcss.config.js",
+  },
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.[jt]sx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+      },
+    },
+  },
+});
